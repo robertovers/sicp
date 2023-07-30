a792d97a24fc063da4fcdaf84a57c64b55fe78a3
Robert Overs <oversr14@gmail.com>
2023-07-30 15:34:50 +1000

    Exercise 1.35

---
+++ 1.3/1.35.scm
@@ -0,0 +1,23 @@
+#| ########################################################################## |#
+#| # The Structure and Interpretation of Computer Programs                  # |#
+#| # ====================================================================== # |#
+#| # Chapter 1: Building Abstractions with Procedures                       # |#
+#| # Section 1.3: Formulating Abstractions with Higher-Order Procedures     # |#
+#| # ====================================================================== # |#
+#| # Exercise 1.35                                                          # |#
+#| ########################################################################## |#
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(display (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.6))
+(newline)
