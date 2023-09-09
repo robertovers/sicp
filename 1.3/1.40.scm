8111ec4ae0768dcdbc1158783062de17bd406fa2
Robert Overs <oversr14@gmail.com>
2023-09-09 17:43:20 +1000

    Exercise 1.40

---
+++ 1.3/1.40.scm
@@ -0,0 +1,43 @@
+#| ########################################################################## |#
+#| # The Structure and Interpretation of Computer Programs                  # |#
+#| # ====================================================================== # |#
+#| # Chapter 1: Building Abstractions with Procedures                       # |#
+#| # Section 1.3: Formulating Abstractions with Higher-Order Procedures     # |#
+#| # ====================================================================== # |#
+#| # Exercise 1.40                                                          # |#
+#| ########################################################################## |#
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (square x) (* x x))
+
+(define (cube x) (* x x x))
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+(display (newtons-method (cubic 4 5 6) 1))
