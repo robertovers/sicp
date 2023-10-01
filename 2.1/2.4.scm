06f6d13604ae28713062c8b65442ddfa890db230
Robert Overs <oversr14@gmail.com>
2023-10-01 18:31:07 +1100

    Exercise 2.4

--- 2.1/2.4.scm
+++ 2.1/2.4.scm
@@ -19,10 +19,10 @@
 #|
 
   (car (cons x y))
-  (car (lambda (m) (m x y)))
-  (lambda (m) (m x y)) (lambda (p q) p)
-  ((lambda (p q) p) x y)
-  x
+  -> (car (lambda (m) (m x y)))
+  -> (lambda (m) (m x y)) (lambda (p q) p)
+  -> ((lambda (p q) p) x y)
+  -> x
 
 |#
 
