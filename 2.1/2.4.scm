b051a82e9bab83f8c02f7753319a1b9b31a8a0b4
Robert Overs <oversr14@gmail.com>
2023-10-01 16:49:44 +1100

    Exercise 2.4

---
+++ 2.1/2.4.scm
@@ -0,0 +1,30 @@
+#| ########################################################################## |#
+#| # The Structure and Interpretation of Computer Programs                  # |#
+#| # ====================================================================== # |#
+#| # Chapter 2: Building Abstractions with Data                             # |#
+#| # Section 2.1: Introduction to Data Abstraction                          # |#
+#| # ====================================================================== # |#
+#| # Exercise 2.4                                                           # |#
+#| ########################################################################## |#
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+#|
+
+  (car (cons x y))
+  (car (lambda (m) (m x y)))
+  (lambda (m) (m x y)) (lambda (p q) p))
+  ((lambda (p q) p) x y)
+  x
+
+|#
+
+(display (car (cons 0 1)))
+(newline)
